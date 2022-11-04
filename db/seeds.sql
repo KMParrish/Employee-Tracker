4c8e6b8b696c426de6bc6e52db041be78403e20c
Kyle Parrish <110362534+KMParrish@users.noreply.github.com>
2022-11-03 21:37:17 -0500

    had to rearrage file structures due to unsolvable errors

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,33 +1,32 @@
-USE employees;
-INSERT INTO department
-    (name)
-VALUES
-    ('Marketing'),
-    ('Finance')
-    ('Operations Management')
-    ('Human Resources'),
-    ('Information Technology'),
-
-INSERT INTO role
-    (title, salary, department_id)
-VALUES
-    ('Software Engineer', 125000, 5),
+INSERT INTO department (name)
+VALUES ('Legal'),
+    ('Sales'),
+    ('Engineering');
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Customer Manager', 90000, 1),
+    ('Lawyer', 170000, 1),
+    ('Sales Director', 180000, 2),
+    ('Salesperson', 70000, 2),
     ('Project Manager', 150000, 3),
-    ('Customer Manager', 90000, 3),
-    ('Sales Director', 180000, 1),
-    ('IT Director', 130000, 5),
-    ('Recruiter', 90000, 4),
-    ('Financial Analyst', 115000, 2),
-    ('Accountant', 75000, 2;
-
-INSERT INTO employee
-    (first_name, last_name, role_id, manager_id)
-VALUES
-    ('Harris', 'McArthur', 1, NULL),
-    ('Kayla', 'Carty', 2, 1),
-    ('Chandler', 'Rankin', 3, NULL),
-    ('Usamah', 'Floyd', 4, 3),
-    ('Jaimee', 'Randall', 7, 3),
-    ('Calista', 'McFarland', 5, NULL),
-    ('Caolan', 'Evans', 8, 5),
-    ('Alister', 'Leigh', 6, 5),
+    ('Engineer', 120000, 3);
+INSERT INTO employee (
+        first_name,
+        last_name,
+        role_id,
+        manager_id,
+        manager_confirm
+    )
+VALUES ('Summer', 'Howe', 1, null, true),
+    ('Manha', 'Flynn', 2, 1, false),
+    ('Connah', 'Gross', 2, 1, false),
+    ('Ajay', 'Jackson', 3, null, true),
+    ('Gerrard', 'Haley', 4, 2, false),
+    ('Avery', 'Stark', 4, 2, false),
+    ('Tony', 'Ramirez', 4, 2, false),
+    ('Nick', 'Walker', 5, null, true),
+    ('Phil', 'Heath', 6, 3, false),
+    ('Andrew', 'Jacked', 6, 3, false);
+INSERT INTO manager (first_name, last_name)
+SELECT first_name,
+    last_name
+FROM employee
